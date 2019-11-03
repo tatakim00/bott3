9a0f965d2a90c3152d508e31436d311bee871334
tatiana <tatianazamuner@icloud.com>
2019-11-03 23:18:14 +0100

    meu bot

--- bot.js
+++ bot.js
@@ -2,5 +2,26 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 
+client.on("ready", () => {
+    console.log('hello');
+    client.user.setGame('Spammare Yoshi');
+});
 
+client.on("guildCreate", guild => {
+    console.log('santa maria')
+    client.user.setActivity('bocchin a mammt')
+});
+
+client.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    const args = message.content.slice(config.prefix.lenght).trim().split(/ +/g);
+    const comando = args.shift().toLowerCase();
+
+    if(comando === "ping") {
+        const m = await message.channel.send("ping?");
+        m.edit('pong!');
+    }
+});
 client.login(config.token)
